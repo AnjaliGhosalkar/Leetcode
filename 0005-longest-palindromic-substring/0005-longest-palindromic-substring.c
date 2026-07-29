8f8eeccb6629043d484b5ca5177a496e2d7be116
AnjaliGhosalkar <anjalighosalkar15@gmail.com>
2026-07-29 21:43:05 +0530

    Time: 8 ms (36.63%), Space: 8.8 MB (92.45%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.c
@@ -0,0 +1,42 @@
+#include <string.h>
+
+char* longestPalindrome(char* s) {
+    int n = strlen(s);
+
+    if (n == 0)
+        return s;
+
+    int start = 0;
+    int maxLen = 1;
+
+    for (int i = 0; i < n; i++) {
+
+        
+        
+        int left = i, right = i;
+        while (left >= 0 && right < n && s[left] == s[right]) {
+            if (right - left + 1 > maxLen) {
+                start = left;
+                maxLen = right - left + 1;
+            }
+            left--;
+            right++;
+        }
+
+        
+        left = i;
+        right = i + 1;
+        while (left >= 0 && right < n && s[left] == s[right]) {
+            if (right - left + 1 > maxLen) {
+                start = left;
+                maxLen = right - left + 1;
+            }
+            left--;
+            right++;
+        }
+    }
+
+    s[start + maxLen] = '\0';
+
+    return s + start;
+}
