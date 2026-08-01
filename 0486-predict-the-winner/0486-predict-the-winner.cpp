35612df7c89be6f752015c58de75d7db3a01e11c
AnjaliGhosalkar <anjalighosalkar15@gmail.com>
2026-08-01 18:34:32 +0530

    Time: 0 ms (100%), Space: 9.6 MB (69.71%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[20][20];
+
+    int solve(vector<int>& nums, int l, int r) {
+        if (l == r)
+            return nums[l];
+
+        if (dp[l][r] != -1)
+            return dp[l][r];
+
+        int takeLeft = nums[l] - solve(nums, l + 1, r);
+        int takeRight = nums[r] - solve(nums, l, r - 1);
+
+        return dp[l][r] = max(takeLeft, takeRight);
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
+
+        return solve(nums, 0, nums.size() - 1) >= 0;
+    }
+};
